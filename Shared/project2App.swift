f24b7ab91099704946399e140e7967c19337bf92
Abdulkarim <Abdulkarim.Alansari@outlook.com>
2021-09-08 19:29:05 +0300

    Initial Commit

---
+++ Shared/project2App.swift
@@ -0,0 +1,17 @@
+//
+//  project2App.swift
+//  Shared
+//
+//  Created by Abdulkarim Alansari on 01/02/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
